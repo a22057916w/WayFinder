17c6a644fde0b2cc69d25ef728cdbae4525d8a17
a22057916w <a22057916w@gmail.com>
2019-09-11 15:48:10 +0800

    Create dumpNext.py

---
+++ Direction/Mysql/dumpNext.py
@@ -0,0 +1,27 @@
+import mysql.connector
+import json
+import re
+import pandas as pd
+import pymysql
+from sqlalchemy import create_engine
+import sys
+# Adding the path of self-def Library
+sys.path.append("C:/Users/w/Documents/GitHub/WayFinder/Direction/Library/script/")
+from featureCollection import Feature, Vertex
+from myio import read_excel
+
+def MYSQL_DUMP_NEXT():
+
+    # ====== Connection ====== #
+    # Connecting to mysql by providing a sqlachemy engine
+    engine = create_engine('mysql+mysqlconnector://root:@140.136.150.100:3306/WayFinder', echo=False)
+
+    # dump dist excel to mysql
+    floorNumber = 9
+    for i in range(0, 9):
+        fileName = "C:\\Users\\w\\Documents\\GitHub\\WayFinder\\Direction\\Route\\next\\sf" + str(i + 1) + "f_next.xlsx"
+        tableName = "sf" + str(i + 1) + "f_next"
+        df = read_excel(fileName)
+        df.to_sql(name = tableName, if_exists="replace", con = engine, index = False)
+
+MYSQL_DUMP_NEXT()
