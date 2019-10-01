9cb30a118582a859998f6ae177cb15d1ee53ace3
a22057916w <a22057916w@gmail.com>
2019-10-01 16:47:07 +0800

    Update sqlInit.py

---
+++ Direction/Mysql/sqlInit.py
@@ -0,0 +1,17 @@
+import multiprocessing as mp
+from dumpDist import MYSQL_DUMP_DIST
+from dumpJson import MYSQL_DUMP_JSON
+from dumpNext import MYSQL_DUMP_NEXT
+from dumpVertex import MYSQL_DUMP_VERTEX
+
+if __name__ == '__main__':
+    func = [MYSQL_DUMP_DIST, MYSQL_DUMP_JSON, MYSQL_DUMP_NEXT, MYSQL_DUMP_VERTEX]
+    n = len(func)
+    p = [None] * n
+    for i in range(0, n):
+        p[i] = mp.Process(target=func[i])
+        p[i].start()
+    for i in range(0, n):
+        p[i].join()
+    for i in range(0, n):
+        p[i].close()
