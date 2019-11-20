227bdf092120e6c4389ac33cd9c9ddebf7d9135f
a22057916w <a22057916w@gmail.com>
2019-11-20 20:26:40 +0800

    change db directory

--- Direction/Library/script/mysqlCoon.py
+++ Direction/Library/script/mysqlCoon.py
@@ -6,5 +6,5 @@ def MY_ENGINE():
 
     # ====== Connection ====== #
     # Connecting to mysql by providing a sqlachemy engin
-    engine = create_engine('mysql+mysqlconnector://root:@140.136.150.100:3306/WayFinder', echo=False)
+    engine = create_engine('mysql+mysqlconnector://root:@140.136.150.100:3306/WayFinder_v2', echo=False)
     return engine
